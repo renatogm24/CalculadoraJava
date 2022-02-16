6c5587c825102fea50e344776c2c13b62df34361
renatogm24 <renato.garay@pucp.pe>
2022-02-15 20:17:11 -0500

    Branch optional

--- Calculadora/src/com/codingdojo/calculadora/Calculadora.java
+++ Calculadora/src/com/codingdojo/calculadora/Calculadora.java
@@ -1,33 +1,86 @@
 package com.codingdojo.calculadora;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+
 public class Calculadora {
 	private double operandoUno;
 	private double operandoDos;
 	private String operacion;
 	private double resultado;
-	
+	private String operacionString;
+
 	public void realizarOperacion() {
 		boolean flag = true;
-		if(this.operacion.equals("+")) {
+		if (this.operacion.equals("+")) {
 			this.resultado = getOperandoUno() + getOperandoDos();
-		}else if(this.operacion.equals("-")) {
+		} else if (this.operacion.equals("-")) {
 			this.resultado = getOperandoUno() - getOperandoDos();
-		}else if(this.operacion.equals("*")) {
+		} else if (this.operacion.equals("*")) {
 			this.resultado = getOperandoUno() * getOperandoDos();
-		}else if(this.operacion.equals("/")) {
+		} else if (this.operacion.equals("/")) {
 			this.resultado = getOperandoUno() / getOperandoDos();
-		}else {
+		} else {
 			System.out.println("No tiene asignado un simbolo de operacion valido");
 			flag = false;
 		}
-		
+
 		if (flag) {
 			System.out.println("Se realizo la operacion");
 		}
 	}
-	
-	public Calculadora() {}
-	
+
+	public void realizarOperacion2(String nuevo) {
+		setOperacionString(getOperacionString().concat(nuevo).concat("ñ"));
+	}
+
+	public void getResultado2() {
+		ArrayList<String> operaciones = new ArrayList<>(Arrays.asList(this.operacionString.split("ñ")));
+
+		for (int i = 0; i < operaciones.size(); i++) {
+			if (operaciones.get(i).equals("*") || operaciones.get(i).equals("/")) {
+				double result = 0;
+				if (operaciones.get(i).equals("*")) {
+					result = Double.parseDouble(operaciones.get(i - 1)) * Double.parseDouble(operaciones.get(i + 1));
+				} else {
+					result = Double.parseDouble(operaciones.get(i - 1)) / Double.parseDouble(operaciones.get(i + 1));
+				}
+				operaciones.set(i, String.valueOf(result));
+				operaciones.remove(i - 1);
+				operaciones.remove(i);
+			}
+		}
+		
+
+		for (int i = 0; i < operaciones.size(); i++) {
+			if (operaciones.get(i).equals("+") || operaciones.get(i).equals("-")) {
+				double result = 0;
+				if (operaciones.get(i).equals("+")) {
+					result = Double.parseDouble(operaciones.get(i - 1)) + Double.parseDouble(operaciones.get(i + 1));
+				} else {
+					result = Double.parseDouble(operaciones.get(i - 1)) - Double.parseDouble(operaciones.get(i + 1));
+				}
+				operaciones.set(i, String.valueOf(result));
+				operaciones.remove(i - 1);
+				operaciones.remove(i);
+			}
+		}
+
+		System.out.println(operaciones.get(0));
+	}
+
+	public String getOperacionString() {
+		return operacionString;
+	}
+
+	public void setOperacionString(String operacionString) {
+		this.operacionString = operacionString;
+	}
+
+	public Calculadora() {
+		operacionString = "";
+	}
+
 	public double getResultado() {
 		System.out.println(resultado);
 		return resultado;
@@ -56,6 +109,5 @@ public class Calculadora {
 	public void setOperacion(String operacion) {
 		this.operacion = operacion;
 	};
-	
-	
+
 }
--- Calculadora/src/com/codingdojo/calculadora/TestCalculadora.java
+++ Calculadora/src/com/codingdojo/calculadora/TestCalculadora.java
@@ -9,6 +9,14 @@ public class TestCalculadora {
 		c.setOperandoDos(5.2);
 		c.realizarOperacion();
 		c.getResultado();
+		
+		Calculadora c2 = new Calculadora();
+		c2.realizarOperacion2("10.5");
+		c2.realizarOperacion2("+");
+		c2.realizarOperacion2("5.2");
+		c2.realizarOperacion2("*");
+		c2.realizarOperacion2("10");
+		c2.getResultado2();
 	}
 
 }
